7eae5b7b92bab81b71a328cf4dbf536463d12341
balroc <baltazarroche@gmail.com>
2023-06-11 21:36:16 -0300

    tp final - problema con firebase

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,6 +1,26 @@
 import './ItemDetail.css'
+import ItemCount from '../ItemCount/ItemCount'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { CarritoContext } from '../../context/CarritoContext'
+import { useContext } from 'react'
 
-const ItemDetail = ({ id, nombre, precio, img, descripcion }) => {
+const ItemDetail = ({ id, nombre, precio, img, descripcion, stock }) => {
+    //1) Se crea estado con cantidad de productos agregados
+    const [addCount, setAddCount] = useState(0);
+
+    //useContext
+    const {agregarProducto} = useContext(CarritoContext);
+
+    //2)Se crea funcion que maneja la cantidad
+    const manejadorCantidad = (cantidad) =>{
+        setAddCount(cantidad);
+        //console.log("Productos agregados:" + cantidad)
+
+        //crear objeto con item y cantidad
+        const item = {id, nombre, precio}
+        agregarProducto(item, cantidad);
+    }
     return (
         <div className='contenedorItem'>
             <h2>Nombre: {nombre}</h2>
@@ -8,6 +28,12 @@ const ItemDetail = ({ id, nombre, precio, img, descripcion }) => {
             <h3>ID: {id}</h3>
             <p>{descripcion}</p>
             <img src={img} alt={nombre} />
+            {
+                //se emplea el montaje y desmontaje de componentes
+            }
+            {
+                addCount > 0 ? (<Link to="/cart"> Terminar compra </Link>) : (<ItemCount inicial={1} stock={stock} funcionAgregar={manejadorCantidad} />)
+            }
         </div>
     )
 }
